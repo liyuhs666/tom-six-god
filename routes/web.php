74565d88da0ef7a96e658d000b8493a17d000848
vagrant <vagrant@homestead>
2017-12-25 04:02:30 +0000

    添加帖子类

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]); // 资源控制器
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

e491d08dcc4bebcee33f1451162916cfec89e2ab
vagrant <vagrant@homestead>
2017-12-26 08:35:30 +0000

    XSS安全漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');	//对数据进行过滤
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }

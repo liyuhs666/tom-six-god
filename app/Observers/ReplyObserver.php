a20473aaf94a1466e430167ad24db7f540bcb6ff
vagrant <vagrant@homestead>
2017-12-28 08:45:53 +0000

    发表回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,14 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        $reply->topic->increment('reply_count', 1);	//每当增加一条评论,评论数+1
     }
 
-    public function updating(Reply $reply)
+    //对入库的评论进行过滤,防止XSS攻击
+    public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
